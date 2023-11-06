843e015f464ee1c75dd4ed05d4b1f8cc59ed1c2c
LMontes94 <monteslucas1994@gmail.com>
2023-11-06 15:37:39 -0300

    funcion obtenerHora y que el reloj se acutalice

--- MUNDO/js/main.js
+++ MUNDO/js/main.js
@@ -127,12 +127,12 @@ document.querySelector('.quality-select3').style.display = 'none';
 function mostrarElemento() {
   // Obtén los elementos con los ID
 
-  
+
   // Verifica si los elementos contienen valores
- 
-    // Muestra el elemento .quality-select3
-    document.querySelector('.quality-select3').style.display = 'block';
-  
+
+  // Muestra el elemento .quality-select3
+  document.querySelector('.quality-select3').style.display = 'block';
+
 }
 
 
@@ -147,41 +147,41 @@ function obtenerDatosPais(countryName) {
   const geonamesUrl = `http://api.geonames.org/countryInfoJSON?name=${countryName}&username=${username}`;
 
   return fetch(geonamesUrl)
-      .then(response => response.json())
-      .then(data => {
-          if (data.geonames && data.geonames.length > 0) {
-              // Filtra el país correcto por nombre
-              const countryInfo = data.geonames.find(country => country.countryName === countryName);
+    .then(response => response.json())
+    .then(data => {
+      if (data.geonames && data.geonames.length > 0) {
+        // Filtra el país correcto por nombre
+        const countryInfo = data.geonames.find(country => country.countryName === countryName);
 
-              if (countryInfo) {
-                  return countryInfo;
-              } else {
-                  throw new Error("No se encontraron resultados para el país especificado.");
-              }
-          } else {
-              throw new Error("No se encontraron resultados para el país especificado.");
-          }
-      })
-      .catch(error => {
-          throw new Error("Error en la solicitud a la API de Geonames: " + error);
-      });
+        if (countryInfo) {
+          return countryInfo;
+        } else {
+          throw new Error("No se encontraron resultados para el país especificado.");
+        }
+      } else {
+        throw new Error("No se encontraron resultados para el país especificado.");
+      }
+    })
+    .catch(error => {
+      throw new Error("Error en la solicitud a la API de Geonames: " + error);
+    });
 }
 
 function obtenerLatitud(countryInfo) {
   if (countryInfo) {
-      const latitud = parseFloat(countryInfo.south);
-      console.log("Latitud:", latitud);
+    const latitud = parseFloat(countryInfo.south);
+    console.log("Latitud:", latitud);
   } else {
-      console.error("No se proporcionaron datos de país.");
+    console.error("No se proporcionaron datos de país.");
   }
 }
 
 function obtenerLongitud(countryInfo) {
   if (countryInfo) {
-      const longitud = parseFloat(countryInfo.west);
-      console.log("Longitud:", longitud);
+    const longitud = parseFloat(countryInfo.west);
+    console.log("Longitud:", longitud);
   } else {
-      console.error("No se proporcionaron datos de país.");
+    console.error("No se proporcionaron datos de país.");
   }
 }
 
@@ -191,7 +191,7 @@ function obtenerContinenteAmericano() {
 
 function reemplazarEspaciosEnCapital(countryInfo) {
   if (countryInfo.capital) {
-      countryInfo.capital = countryInfo.capital.replace(/\s/g, "_");
+    countryInfo.capital = countryInfo.capital.replace(/\s/g, "_");
   }
   return countryInfo;
 }
@@ -208,10 +208,6 @@ function obtenerFechaDesdeDatetime(datetimeString) {
   return { dia, mes, anio };
 }
 
-function formatearHora(hora, minutos, segundos) {
-  return `${hora.toString().padStart(2, '0')} : ${minutos.toString().padStart(2, '0')} : ${segundos.toString().padStart(2, '0')}`;
-}
-
 function formatearFecha(dia, mes, anio) {
   return `${dia.toString().padStart(2, '0')} / ${mes.toString().padStart(2, '0')} / ${anio}`;
 }
@@ -223,43 +219,60 @@ function limpiarInput() {
   document.getElementById("country").value = "";
 }
 
+function limpiarClock(intervalo) {
+  if (intervalo) {
+    clearInterval(intervalo);
+    intervalo = null;
+  }
+  document.getElementById("clock").value = ""
+}
+
+function obtenerHora(timezone) {
+  const dia = new Date();
+  const hora = dia.toLocaleTimeString("default", { timeZone: timezone });
+  return hora;
+}
+let intervalo;
 function updateClock(continentName, countryName, capital) {
-  const clockElement = document.getElementById("clock");
   const day = document.getElementById("day");
+  const clockElement = document.getElementById("clock");
 
+  limpiarClock(intervalo);
   try {
-      let url;
-      if (countryName == "Argentina") {
-          url = `https://worldtimeapi.org/api/timezone/${continentName}/${countryName}/${capital}`;
-      } else {
-          url = `https://worldtimeapi.org/api/timezone/${continentName}/${capital}`;
-      }
+    let url;
+    if (countryName == "Argentina") {
+      url = `https://worldtimeapi.org/api/timezone/${continentName}/${countryName}/${capital}`;
+    } else {
+      url = `https://worldtimeapi.org/api/timezone/${continentName}/${capital}`;
+    }
 
-      fetch(url)
-          .then(response => response.json())
-          .then(data => {
-              console.log(data)
-              if (data.datetime) {
-                  const dateTimeString = data.datetime;
-                  const hora = dateTimeString.match(/T(\d{2}:\d{2}:\d{2})/)[1];/*la expresión regular /T(\d{2}:\d{2}:\d{2})/ busca el patrón "T" 
-                                                                               seguido de "HH:MM:SS" en la cadena dateTimeString y el [1] extrae la parte de la hora. */
-                  const fechaCompleta = obtenerFechaDesdeDatetime(dateTimeString);
-                  console.log(fechaCompleta);
-                  console.log(hora);
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data.datetime) {
+          const dateTimeString = data.datetime;
+          //const hora = dateTimeString.match(/T(\d{2}:\d{2}:\d{2})/)[1];/*la expresión regular /T(\d{2}:\d{2}:\d{2})/ busca el patrón "T" 
+          //                                                                     seguido de "HH:MM:SS" en la cadena dateTimeString y el [1] extrae la parte de la hora. */
+          const fechaCompleta = obtenerFechaDesdeDatetime(dateTimeString);
+          console.log(fechaCompleta);
 
-                  day.textContent = mostrarFecha(fechaCompleta);
-                  clockElement.textContent = hora;
-                  
-              } else {
-                  throw new Error("Error al obtener la hora local.");
-              }
-          })
-          .catch(error => {
-              console.error("Error al obtener la hora local:", error);
-          });
+          day.textContent = mostrarFecha(fechaCompleta);
+
+          intervalo = setInterval(() => {
+            clockElement.textContent = obtenerHora(data.timezone);
+            console.log(obtenerHora(data.timezone))
+          }, 1000);
+        } else {
+          throw new Error("Error al obtener la hora local.");
+        }
+      })
+      .catch(error => {
+        console.error("Error al obtener la hora local:", error);
+      });
   } catch (error) {
-      console.error("Error al obtener la hora local:", error);
-      // Puedes mostrar un mensaje de error en el reloj o realizar alguna otra acción en caso de error
+    console.error("Error al obtener la hora local:", error);
+    // Puedes mostrar un mensaje de error en el reloj o realizar alguna otra acción en caso de error
   }
 }
 
@@ -267,34 +280,34 @@ const updateButton = document.getElementById("update-button");
 updateButton.addEventListener("click", async () => {
   const countryName = obtenerPais();
   if (countryName) {
-      obtenerDatosPais(countryName)
-          .then(countryInfo => {
-              console.log(countryInfo)
-              obtenerLatitud(countryInfo);
-              obtenerLongitud(countryInfo);
-              let continentName;
-              if (countryInfo.continentName.includes("America")) {
-                  continentName = obtenerContinenteAmericano(countryInfo)
-                  console.log(continentName);
-              } else {
-                  continentName = countryInfo.continentName;
-              }
-              console.log(countryName)
-              reemplazarEspaciosEnCapital(countryInfo);
-              const capital = quitarTildes(countryInfo.capital);
-              console.log(capital);
-              const name = document.getElementById("nombre");
-              name.textContent = countryName;
-              limpiarInput();
-              mostrarElemento();              
-              updateClock(continentName, countryName, capital);
-              
-          })
-          .catch(error => {
-              alert(error);
-          });
+    obtenerDatosPais(countryName)
+      .then(countryInfo => {
+        console.log(countryInfo)
+        obtenerLatitud(countryInfo);
+        obtenerLongitud(countryInfo);
+        let continentName;
+        if (countryInfo.continentName.includes("America")) {
+          continentName = obtenerContinenteAmericano(countryInfo)
+          console.log(continentName);
+        } else {
+          continentName = countryInfo.continentName;
+        }
+        console.log(countryName)
+        reemplazarEspaciosEnCapital(countryInfo);
+        const capital = quitarTildes(countryInfo.capital);
+        console.log(capital);
+        const name = document.getElementById("nombre");
+        name.textContent = countryName;
+        limpiarInput();
+        mostrarElemento();
+        updateClock(continentName, countryName, capital);
+
+      })
+      .catch(error => {
+        alert(error);
+      });
   } else {
-      alert("Por favor, ingresa un país válido.");
+    alert("Por favor, ingresa un país válido.");
   }
 });
 
